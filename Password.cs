f5473e8f40cde053e93d7204e29441d2933898a6
sarthakbatra708 <88719272+sarthakbatra708@users.noreply.github.com>
2023-09-08 14:37:16 +0530

    Add files via upload

---
+++ Password.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userid;
+            string password;
+            Console.WriteLine("Enter userid:");
+            userid = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            password = Console.ReadLine();
+            if (userid == "Sarthak" && password == "hello123")
+            {
+                Console.WriteLine("Successfully Logged In");
+            }
+            else
+            {
+                Console.WriteLine("No user with the given credentials");
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ product_details.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            float ProductId ,UnitPrice ,Quantity,Discount,Total,Discountamt,finalamt ;
+            string ProductName;
+            Console.WriteLine("Enter Product ID");
+           
+            ProductId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Product Name");
+            ProductName = Console.ReadLine();
+            Console.WriteLine(      "Enter Unit Price");
+           
+            UnitPrice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Product Qty");
+            
+            Quantity = Convert.ToInt32(Console.ReadLine());
+
+            if (Quantity >= 50)
+            {
+                Discount = 30;
+                Total = UnitPrice * Quantity;
+                Discountamt= ((Discount/100)*Total);
+                finalamt = Total - ((Discount / 100) * Total);
+               
+            }
+            else if(Quantity >=30 )
+            {
+                Discount = 20;
+                Total = UnitPrice * Quantity;
+                Discountamt = ((Discount / 100) * Total);
+                finalamt = Total - ((Discount / 100) * Total);
+            }
+            else if(Quantity>=10)
+            {
+                Discount=10;
+                Total = UnitPrice * Quantity;
+                Discountamt = ((Discount / 100) * Total);
+                finalamt = Total - ((Discount / 100) * Total); 
+            }
+            else
+            {
+                Total = UnitPrice * Quantity;
+                Discountamt = 0;
+                finalamt =Total;
+
+            }
+           
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Product Id : "+ProductId);
+            Console.WriteLine("Product Name : " + ProductName);
+            Console.WriteLine("Unit Price : " + UnitPrice);
+            Console.WriteLine("Quantity : " + Quantity);
+            Console.WriteLine("Total Amount : " + Total);
+            Console.WriteLine("Discount Amount : " + Discountamt);
+            Console.WriteLine("Final Amount : " + finalamt);
+            Console.ReadLine();
+
+        }
+    }
+}
