c3b7963cb8f55de4781bd5008187e73d52b26618
sarthakbatra708 <88719272+sarthakbatra708@users.noreply.github.com>
2023-09-08 14:42:22 +0530

    Add files via upload

---
+++ StarPassword.cs
@@ -0,0 +1,11 @@
+﻿string pwd = "";
+do
+{
+    key = Console.ReadKey(true);
+    if (keey.Key != ConsoleKey.Enter)
+    {
+        strPassword += key.KeyChar;
+        Console.Write("*");
+    }
+}
+while (key.Key != ConsoleKey.Enter);
