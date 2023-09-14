9b038ad6032c9508cb56fc6a6f4cb54cc93345b1
sarthakbatra708 <88719272+sarthakbatra708@users.noreply.github.com>
2023-09-14 11:36:28 +0530

    EntityModelClasses.cs

---
+++ WebApi/EntityModelClasses.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication4.Models
+{
+    public class Product
+    {
+       
+            public int ProductId { get; set; }
+            public string ProductName { get; set; }
+            public int Quantity { get; set; }
+            public int UnitPrice { get; set; }
+        public string Category { get; set; }
+    }
+
+        public class ProductDbContext : DbContext
+        {
+            public DbSet<Product> Products { get; set; }
+
+            public ProductDbContext(DbContextOptions<ProductDbContext> options)
+             : base(options)
+            {
+
+            }
+        }
+    }
