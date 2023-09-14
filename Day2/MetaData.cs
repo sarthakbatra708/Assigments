c171ac3f1589b0f5de2ca6fb669770281ae76ef8
sarthakbatra708 <88719272+sarthakbatra708@users.noreply.github.com>
2023-09-14 09:56:05 +0530

    MetaData.cs

---
+++ Day2/MetaData.cs
@@ -0,0 +1,26 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\\Users\sarbatra\\\\source\\repos\\ConsoleApp4\\ConsoleApp4\\Program.cs";
+
+            FileInfo file = new FileInfo(filepath);
+
+            Console.WriteLine("File Name:{0}\nFile Size:{1}\nCreation Time:{2}", file.Name, file.Length, file.CreationTime);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
