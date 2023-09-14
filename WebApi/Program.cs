2d2fc79db70794c1477d48a8aec831fbf58f2125
sarthakbatra708 <88719272+sarthakbatra708@users.noreply.github.com>
2023-09-14 11:37:57 +0530

    Update Program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<PProductDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PProductDbContext"))); ;
+builder.Services.AddDbContext<ProductDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductDbContext"))); ;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
