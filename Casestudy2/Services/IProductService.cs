7e73cf8a98167205e9a9ef1cd06fb340cd64037b
sarthakbatra708 <88719272+sarthakbatra708@users.noreply.github.com>
2023-09-19 17:28:47 +0530

    Create IProductService.cs

---
+++ Casestudy2/Services/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using CaseStudy2.Models;
+
+namespace CaseStudy2.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+        Product Create(Product product);
+        void Edit(Product product);
+        void Delete(int id);
+        Product GetProductById(int id);
+        List<Product> GetProductsByCategory(string category);
+        List<Product> GetOutOfStockProducts();
+        List<Product> GetProductsInRangeOfPrice(double start ,double end);
+        List<string> GetCategories();
+
+    }
+}
