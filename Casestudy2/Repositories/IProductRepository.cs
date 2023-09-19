a99397b4c24152f299f83b7519250d481f561d01
sarthakbatra708 <88719272+sarthakbatra708@users.noreply.github.com>
2023-09-19 17:25:55 +0530

    Create IProductRepository.cs

---
+++ Casestudy2/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using CaseStudy2.Models;
+namespace CaseStudy2.Repositories
+{
+    public interface IProductRepository
+    {
+        List<Product> GetProducts();
+        Product Create(Product product);
+        void Edit(Product product);
+        void Delete(int id);
+        Product GetProductById(int id);
+    }
+}
