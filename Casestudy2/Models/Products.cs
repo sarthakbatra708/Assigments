5f4615b7b236e5cef0dc6db4d9c0878c10c644b7
sarthakbatra708 <88719272+sarthakbatra708@users.noreply.github.com>
2023-09-19 17:22:49 +0530

    Create Products.cs

---
+++ Casestudy2/Models/Products.cs
@@ -0,0 +1,13 @@
+﻿namespace CaseStudy2.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double UnitPrice { get; set; }
+        public int Quantity {  get; set; }
+        public string Description { get; set; }
+
+    }
+}
